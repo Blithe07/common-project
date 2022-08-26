47af2b0c5f17f0e98ce95e0de784599cfbf6cc81
laitongbin <laitongbin@yhfin.com>
2022-08-26 15:14:01 +0800

    test: add unit test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-essential',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ],
-  overrides: [],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['vue', '@typescript-eslint', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error'
-  }
-}
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+    ],
+    overrides: [],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.eslint.json"
+    },
+    plugins: ["vue", "@typescript-eslint", "prettier"],
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {},
+            {
+                fileInfoOptions: {
+                    withNodeModules: true
+                }
+            }
+        ]
+    },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 const calc = (a: number, b: number) => {
-  return a - b
-}
-// console.log(calc(1024, 28))
+    return a - b;
+};
+console.log(calc(1024, 28));
+export default calc;
+// git提交规范(提供交互界面) npx commitizen init cz-conventional-changelog --save-dev --save-exact
